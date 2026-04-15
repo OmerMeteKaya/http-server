32cc87694e0ef31a91ee4fdd942fc9781e8ea7cf
Omer Mete Kaya <omermetekaya0@gmail.com>
2026-04-15 22:13:28 +0300

    Multithreadding added

--- ThreadPool.c
+++ ThreadPool.c
@@ -3,3 +3,115 @@
 //
 
 #include "ThreadPool.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void *worker(void *arg)
+{
+    ThreadPool *pool = (ThreadPool *)arg;
+
+    while (1)
+    {
+        pthread_mutex_lock(&pool->lock);
+
+
+        while (pool->queue_size == 0 && !pool->stop)
+        {
+            pthread_cond_wait(&pool->notify, &pool->lock);
+        }
+
+        if (pool->stop)
+        {
+            pthread_mutex_unlock(&pool->lock);
+            break;
+        }
+
+
+        ThreadJob job = pool->queue[pool->queue_front];
+        pool->queue_front = (pool->queue_front + 1) % MAX_QUEUE;
+        pool->queue_size--;
+
+        pthread_mutex_unlock(&pool->lock);
+
+
+        job.function(job.arg);
+    }
+
+    return NULL;
+}
+
+
+ThreadPool thread_pool_init(int num_threads)
+{
+    ThreadPool pool;
+
+    pool.thread_count = num_threads;
+    pool.queue_size = 0;
+    pool.queue_front = 0;
+    pool.queue_rear = 0;
+    pool.stop = 0;
+
+    pool.threads = malloc(sizeof(pthread_t) * num_threads);
+
+    pthread_mutex_init(&pool.lock, NULL);
+    pthread_cond_init(&pool.notify, NULL);
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_create(&pool.threads[i], NULL, worker, &pool);
+    }
+
+    return pool;
+}
+
+
+void thread_pool_add_job(ThreadPool *pool, ThreadJob job)
+{
+    pthread_mutex_lock(&pool->lock);
+
+    if (pool->queue_size == MAX_QUEUE)
+    {
+        printf("Queue full!\n");
+        pthread_mutex_unlock(&pool->lock);
+        return;
+    }
+
+    pool->queue[pool->queue_rear] = job;
+
+    pool->queue_rear = (pool->queue_rear + 1) % MAX_QUEUE;
+    pool->queue_size++;
+
+    pthread_cond_signal(&pool->notify);
+    pthread_mutex_unlock(&pool->lock);
+}
+
+
+
+ThreadJob thread_job_create(void (*function)(void *), void *arg)
+{
+    ThreadJob job;
+    job.function = function;
+    job.arg = arg;
+    return job;
+}
+
+
+
+void thread_pool_destroy(ThreadPool *pool)
+{
+    pthread_mutex_lock(&pool->lock);
+    pool->stop = 1;
+    pthread_cond_broadcast(&pool->notify);
+    pthread_mutex_unlock(&pool->lock);
+
+    for (int i = 0; i < pool->thread_count; i++)
+    {
+        pthread_join(pool->threads[i], NULL);
+    }
+
+    free(pool->threads);
+
+    pthread_mutex_destroy(&pool->lock);
+    pthread_cond_destroy(&pool->notify);
+}
--- ThreadPool.h
+++ ThreadPool.h
@@ -2,7 +2,43 @@
 // Created by mete on 14.04.2026.
 //
 
-#ifndef HTTP_SERVER_THREADPOOL_H
-#define HTTP_SERVER_THREADPOOL_H
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
 
-#endif //HTTP_SERVER_THREADPOOL_H
+#include <pthread.h>
+
+#define MAX_QUEUE 1024
+
+
+typedef struct ThreadJob {
+    void (*function)(void *);
+    void *arg;
+} ThreadJob;
+
+
+typedef struct ThreadPool {
+    pthread_t *threads;
+    int thread_count;
+
+    ThreadJob queue[MAX_QUEUE];
+    int queue_size;
+    int queue_front;
+    int queue_rear;
+
+    pthread_mutex_t lock;
+    pthread_cond_t notify;
+
+    int stop;
+} ThreadPool;
+
+
+ThreadPool thread_pool_init(int num_threads);
+
+
+void thread_pool_add(ThreadPool *pool, void (*function)(void *), void *arg);
+void thread_pool_add_job(ThreadPool *pool, ThreadJob job);
+
+ThreadJob thread_job_create(void (*function)(void *), void *arg);
+void thread_pool_destroy(ThreadPool *pool);
+
+#endif
